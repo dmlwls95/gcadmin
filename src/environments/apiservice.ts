f478630e364cd80f8e9b9e145f5efc38a54b4cfd
uijin <uijin95@gmail.com>
2019-05-10 10:05:12 +0900

    cafev4

--- src/environments/apiservice.ts
+++ src/environments/apiservice.ts
@@ -1,1 +1,1 @@
-export const apiurl = 'http://gcserver.cafe24app.com:8001' //'https://gcmean.herokuapp.com' 'http://localhost:4000' //'http://121.136.243.232:4000';
+export const apiurl = 'http://gcserver.cafe24app.com' //'https://gcmean.herokuapp.com' 'http://localhost:4000' //'http://121.136.243.232:4000';
