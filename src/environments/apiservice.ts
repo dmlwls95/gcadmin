c182b9d9f001304b034c3a075825aa6339df7862
uijin <uijin95@gmail.com>
2019-06-01 14:31:03 +0900

    api

--- src/environments/apiservice.ts
+++ src/environments/apiservice.ts
@@ -1,1 +1,1 @@
-export const apiurl = 'http://localhost:4000' //'https://gcmean.herokuapp.com' 'http://localhost:4000' //'http://121.136.243.232:4000';
+export const apiurl = 'http://1.238.177.122:4000' //'https://gcmean.herokuapp.com' 'http://localhost:4000' //'http://121.136.243.232:4000';
